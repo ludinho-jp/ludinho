f0fc7b89b9f05084998c3e697ec4827a0ff9cd5a
dev-peu <pedrocadast@gmail.com>
2023-01-12 16:34:00 -0300

    last onfigs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import LudinhoRoutes from "./routes/routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router>
     <LudinhoRoutes />
   </Router>
 );
