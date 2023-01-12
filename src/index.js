6161ced279caca2f4153af3186f1a10f6292ccc5
dev-peu <pedrocadast@gmail.com>
2023-01-12 16:25:37 -0300

    more configs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import LudinhoRoutes from "./routes/routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <Router basename={process.env.PUBLIC_URL}>
     <LudinhoRoutes />
-  </HashRouter>
+  </Router>
 );
